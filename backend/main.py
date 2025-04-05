ab4109c480cb0acf077f34e77dd280bc0d0745b6
Jeet <83248565+TangibleTensor@users.noreply.github.com>
2025-04-05 23:36:05 +0100

    implemented CORS

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routes import game
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 # uvicorn app --reload
 
@@ -10,7 +11,14 @@ app = FastAPI()
 app.include_router(game.router, prefix="/game", tags=["Game"])
 
 def run_app():
-    """Run the FastAPI app"""
+    app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=["*"],
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+    
     return app
 
 if __name__ == "__main__":
