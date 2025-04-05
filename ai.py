3229bd55cc9c9571c702f47d78155ad77167985b
svn27 <svn27@bath.ac.uk>
2025-04-05 14:59:48 +0100

    starting out

--- ai.py
+++ ai.py
@@ -1,1 +1,24 @@
-print("Hello world"!)
+import spacy
+
+nlp = spacy.load("en_core_web_lg")
+
+
+def similarity(word1, word2):
+    return word1.similarity(word2)
+
+
+words = [
+    "apple",
+    "cat",
+    "america",
+    "mexico",
+    "aeroplane",
+    "food",
+    "orange",
+    "whiteboard",
+]
+
+key = nlp("animal")
+
+for word in words:
+    print("animal", word, similarity(key, nlp(word)))
