f360489c13cf318942018f1ac379430805e60adc
svn27 <svn27@bath.ac.uk>
2025-04-05 15:00:09 +0100

    starting out

--- ai.py
+++ ai.py
@@ -7,18 +7,18 @@ def similarity(word1, word2):
     return word1.similarity(word2)
 
 
-words = [
-    "apple",
-    "cat",
-    "america",
-    "mexico",
-    "aeroplane",
-    "food",
-    "orange",
-    "whiteboard",
-]
+# words = [
+#     "apple",
+#     "cat",
+#     "america",
+#     "mexico",
+#     "aeroplane",
+#     "food",
+#     "orange",
+#     "whiteboard",
+# ]
 
-key = nlp("animal")
+# key = nlp("animal")
 
-for word in words:
-    print("animal", word, similarity(key, nlp(word)))
+# for word in words:
+#     print("animal", word, similarity(key, nlp(word)))
