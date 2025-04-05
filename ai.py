91df588317e34877670a8bd221f5dd3a503313de
rikhil05 <rikhil2005@gmail.com>
2025-04-05 16:04:46 +0100

    ai guesser push

--- ai.py
+++ ai.py
@@ -2,23 +2,31 @@ import spacy
 
 nlp = spacy.load("en_core_web_lg")
 
-
 def similarity(word1, word2):
     return word1.similarity(word2)
 
+def ai_guesser(board, guess_word, n):
+    guess_word = nlp(guess_word)
+    s = {}
+    
+    for word in board:
+        s[word] = similarity(guess_word, nlp(word))
+    
+    keys = list(s.keys())
+    keys.sort(key = lambda x: s[x], reverse=True) 
+    
+    return keys[0:n]
+        
+words = [
+    "apple",
+    "cat",
+    "america",
+    "mexico",
+    "aeroplane",
+    "food",
+    "orange",
+    "whiteboard",
+    "bird"
+]
 
-# words = [
-#     "apple",
-#     "cat",
-#     "america",
-#     "mexico",
-#     "aeroplane",
-#     "food",
-#     "orange",
-#     "whiteboard",
-# ]
-
-# key = nlp("animal")
-
-# for word in words:
-#     print("animal", word, similarity(key, nlp(word)))
+print(ai_guesser(words, "eagle", 2))
