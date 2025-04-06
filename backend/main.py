636d4f47b87b7e8a3dc4d1c7e1b69c2c6a8cf9d7
Jeet <83248565+TangibleTensor@users.noreply.github.com>
2025-04-06 01:44:58 +0100

    commit tax fraud

--- backend/main.py
+++ backend/main.py
@@ -7,18 +7,18 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# Include the routes
-app.include_router(game.router, prefix="/game", tags=["Game"])
-
-def run_app():
-    app.add_middleware(
+app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
     allow_origins=["*"],
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
 )
-    
+
+# Include the routes
+app.include_router(game.router, prefix="/game", tags=["Game"])
+
+def run_app():
     return app
 
 if __name__ == "__main__":
