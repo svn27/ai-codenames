54fa1ccf1e52d59ad3a878d7b4fea8202f13f408
Jeet <83248565+TangibleTensor@users.noreply.github.com>
2025-04-06 02:52:37 +0100

    updated ai guesses endpoint with pydantic model

--- backend/models/misc.py
+++ backend/models/misc.py
@@ -4,3 +4,12 @@ from typing import List
 class SpymasterRequest(BaseModel):
     all_words: List[str]
     ai_words: List[str]
+    
+class SpymasterRequest(BaseModel):
+    all_words: List[str]
+    ai_words: List[str]
+    
+class OperatorRequest(BaseModel):
+    all_words: List[str]
+    spymaster_word: str
+    goes: int
